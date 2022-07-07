e6e942b981b9993a1f6ca0c399064734546e9dd2
MauriRo <106283620+MauriRo@users.noreply.github.com>
2022-07-07 17:22:03 -0500

    Unidad III. Tarea 2

---
+++ Practicas php/index2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+    $x = 30;
+    $y = 16;
+
+    echo $x + $y;
+
+    echo "<br>"
+      
+    ?>
+    <?php 
+
+    $x = 5.3;
+    $y = 6.1;
+
+    echo $x + $y; 
+
+    echo "<br>"
+    ?>
+
+<?php 
+
+$x = 6;
+$y = 5;
+
+echo $x * $y; 
+
+echo "<br>"
+
+?>
+
+<?php
+$var1='Mauricio';
+$var2=' 6';
+$texto_completo = $var1 . $var2;
+echo $texto_completo;
+
+?>
+
+ </body>
+</html> 
